a20d0516843c90e4d18b8aa4c9b2119e40555340
alanchoo47 <alanchoo47@gmail.com>
2022-05-22 18:33:28 -0500

    Initial commit

---
+++ app/src/main/java/com/alancontreras/lectorqr/QR.kt
@@ -0,0 +1,10 @@
+package com.alancontreras.lectorqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

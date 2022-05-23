03243121966f4b540a7a9552b7fd93568fa5fdb4
alanchoo47 <alanchoo47@gmail.com>
2022-05-22 23:55:35 -0500

    Se dividieron los casos para trabajar Correo, sms, vcard y URL. sin  embargo no se logro conseguir el tipo de URI de cada uno para poder diferenciarlos y asi trabajar en caso de que scannerView leyera un URL con ciertas caracteristicas
    La lógica es simple, un when con 4 casos y un default.
    
    Otra forma de realizarlo es mediante 4 botones y su respectivo OnClick. se realizan el intent dependiendo de cada caso y se lee el codigo QR.
    En caso de fallo, regresar a la pantalla con los 4 botones.

--- app/src/main/java/com/alancontreras/lectorqr/QR.kt
+++ app/src/main/java/com/alancontreras/lectorqr/QR.kt
@@ -1,15 +1,28 @@
 package com.alancontreras.lectorqr
 
 import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.UriMatcher
 import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.util.MalformedJsonException
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.Result
+import com.google.zxing.client.result.URIParsedResult
+import com.google.zxing.client.result.URIResultParser
+import me.dm7.barcodescanner.core.BarcodeScannerView
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import java.net.URL
 
 
 class QR : AppCompatActivity(), ZXingScannerView.ResultHandler{
@@ -46,8 +59,41 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler{
     override fun handleResult(p0: Result?) {
         //codigo QR leido
         val scanResult = p0?.text
-
+        var a = 0
         Log.d("QR_LEIDO", scanResult!!)
+        var type = scanResult
+        try{
+
+            /*
+            Se realiza un when para diferencias los casos y asi trabajar en dado caso que se requiera, usando el MIME de cado uno
+            text/plain es para mensjae de texto
+            text/html es para url
+            http.plain_text_type es para correo electronico
+
+             */
+            var i: Intent = when(type){
+                "text/plain" -> MensajeSMS(scanResult)
+                "text/html" -> DirUrl(scanResult)
+                "HTTP.PLAIN_TEXT_TYPE" -> MensajeCorreo(scanResult)
+                else ->  nada(scanResult)
+            }
+
+            i.setData(Uri.parse(scanResult))
+            startActivity(i)
+            finish()
+
+        }catch(e: MalformedJsonException){
+
+            AlertDialog.Builder( this@QR)
+                .setTitle("Error")
+                .setMessage("El codigo QR no es valido para la aplicacion")
+                .setPositiveButton("Aceptar", DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    finish()
+                })
+                .create()
+                .show()
+        }
 
     }
 
@@ -69,4 +115,72 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler{
         super.onDestroy()
         scannerView?.stopCamera()
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode ){
+            PERMISO_CAMARA -> {
+                if(grantResults.isNotEmpty()){
+                    if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                                AlertDialog.Builder(this@QR)
+                                    .setTitle("Permiso requerido")
+                                    .setMessage(("Se necesita acceder a la cámara para leer los códigos QR"))
+                                    .setPositiveButton("Aceptar", DialogInterface.OnClickListener{ dialogInterface, i ->
+                                        requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISO_CAMARA)
+                                    })
+                                    .setNegativeButton("Cancelar", DialogInterface.OnClickListener { dialogInterface, i ->
+                                        dialogInterface.dismiss()
+                                        finish()
+                                    })
+                                    .create()
+                                    .show()
+                            }else{
+                                Toast.makeText(this@QR, "El permiso de la cámara no se ha concedido", Toast.LENGTH_LONG).show()
+                                finish()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun MensajeSMS(scanResult: String?): Intent{
+    var i = Intent(Intent.ACTION_SEND).apply {
+        data = Uri.parse("smsto:")
+        putExtra("sms_body",scanResult)
+    }
+    i.setData(Uri.parse(scanResult))
+    return i
+}
+fun MensajeCorreo(scanResult: String?): Intent{
+    var i = Intent(Intent.ACTION_SEND).apply {
+        data = Uri.parse("mailto:")
+        putExtra(Intent.EXTRA_EMAIL, scanResult)
+        putExtra(Intent.EXTRA_SUBJECT, scanResult)
+        putExtra(Intent.EXTRA_TEXT,scanResult)
+
+    }
+    i.setData(Uri.parse(scanResult))
+    return i
+}
+fun DirUrl(scanResult: String?): Intent{
+    var url = URL(scanResult)
+    var i = Intent(Intent.ACTION_VIEW)
+    i.setData(Uri.parse(scanResult))
+    return i
+}
+fun nada(scanResult: String?): Intent{
+    var url = URL(scanResult)
+    var i = Intent(Intent.ACTION_VIEW)
+    i.setData(Uri.parse(scanResult))
+    return i
 }

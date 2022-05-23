7abcd7434baf4ab30e22892a298a024bb48c2e2d
alanchoo47 <alanchoo47@gmail.com>
2022-05-22 19:24:30 -0500

    Se contemplo el ciclo de vida de la aplicación, por lo que en dado caso que se quite los permisos durante la ejecución de la aplicación, se tomarán las medidas necesarias. En caso similar, cuando se detiene.

--- app/src/main/java/com/alancontreras/lectorqr/QR.kt
+++ app/src/main/java/com/alancontreras/lectorqr/QR.kt
@@ -1,10 +1,16 @@
 package com.alancontreras.lectorqr
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+
 
 class QR : AppCompatActivity(), ZXingScannerView.ResultHandler{
 
@@ -17,8 +23,24 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler{
         scannerView = ZXingScannerView( this )
         setContentView(scannerView)
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checarPermiso()){
+                //se concedio el permiso
+            }else{
+                solicitarPermiso()
+            }
+        }
+
         scannerView?.setResultHandler (this)
         scannerView?.startCamera()
+    }
+
+    private fun solicitarPermiso() {
+        ActivityCompat.requestPermissions(this@QR, arrayOf(Manifest.permission.CAMERA), PERMISO_CAMARA)
+    }
+
+    private fun checarPermiso(): Boolean {
+        return(ContextCompat.checkSelfPermission(this@QR, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
     }
 
     override fun handleResult(p0: Result?) {
@@ -27,5 +49,24 @@ class QR : AppCompatActivity(), ZXingScannerView.ResultHandler{
 
         Log.d("QR_LEIDO", scanResult!!)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checarPermiso()){
+                if(scannerView == null) {
+                    scannerView = ZXingScannerView(this)
+                    setContentView(scannerView)
+                }
+                scannerView?.setResultHandler (this)
+                scannerView?.startCamera()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scannerView?.stopCamera()
     }
 }
